f9d0e26a37fbeae8154bf12822fdfddeab4fc249
pmendezalva1 <46577116+pmendezalva1@users.noreply.github.com>
2019-03-21 10:38:57 -0600

    CSNet Validation

---
+++ CSNet/NorthwindSystem/BLL/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using NorthwindSystem.Data; //the .data class
+using NorthwindSystem.DAL; //the DAL context class
+#endregion
+
+namespace NorthwindSystem.BLL
+{
+    //The classes within the BLL are referred to as your interface. These classes will be xalled by your webapp. They're public.
+    //For ease of maintenance, each class will represent a specific data class, ie. Product.
+
+    public class ProductController
+    {
+        //Create a method to find a specific record on the SQL table. This will be done by the primary key.
+        //Input: search arg value, output: results of the search > single Product record for this case.
+        //This method is public.
+        public Product Product_Get(int productid)
+        {
+            //Connect to the appropriate context class to access the db.
+            //Create an instance of the appropriate context class.
+            //Accessing the appropriate DBSet<T>, issue a request for execution against the SQL table.
+            //The appropriate request return data will be received. 
+            //Return these results. 
+            //The request will be done in a transaction. For read-only, we don't need to worry.
+            //This lookup is by PRIMARY KEY.
+            using (var context = new NorthwindContext())
+            {
+                return context.Products.Find(productid);
+            }
+        }
+
+        //Obtain a list of all table records.
+        //Input: None
+        //Output: List<T> where <T> is the appropriate data class (Product)
+
+        public List<Product> Product_List()
+        {
+            using (var context = new NorthwindContext())
+            {
+                return context.Products.ToList(); //Extensions of the DBSet class. Built in.
+            }
+        }
+    }
+}
---
+++ CSNet/WebApp/SamplePages/SimpleQueries.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+#region Additional Namespaces
+using NorthwindSystem.BLL; //Controller class
+using NorthwindSystem.Data; //Data definition class
+
+#endregion
+
+namespace WebApp.SamplePages
+{
+    public partial class SimpleQueries : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Clear out old messages.
+            MessageLabel.Text = "";
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            int productid = 0;
+
+            //Validation
+            if (string.IsNullOrEmpty(SearchArg.Text.Trim()))
+            {
+                MessageLabel.Text = "Enter a product ID to search.";
+            }
+            else if (int.TryParse(SearchArg.Text.Trim(), out productid))
+            {
+                //Good: Process database request
+                try
+                {
+                    //Connect to BLL controller
+                    ProductController sysmgr = new ProductController();
+                    //Issue request to controller
+                    Product results = sysmgr.Product_Get(int.Parse(SearchArg.Text.Trim())); //Or just use productid instead of the parse
+                    //Check results: Single record check == null
+                    if (results == null)
+                    {
+                        //If None: message to user
+                        MessageLabel.Text = "No data found for supplied search value.";
+                    }
+                    else
+                    {
+                        //Found: Display data
+                        ProductID.Text = results.ProductID.ToString();
+                        ProductName.Text = results.ProductName;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    //Bad: Message to user
+                    MessageLabel.Text = ex.Message;
+                }
+            }
+            else
+            {
+                //Bad: Message to user
+                MessageLabel.Text = "Product ID is not a number greater than 0.";
+            }
+
+        }
+
+        protected void Clear_Click(object sender, EventArgs e)
+        {
+            SearchArg.Text = "";
+            ProductID.Text = "";
+            ProductName.Text = "";
+        }
+    }
+}
