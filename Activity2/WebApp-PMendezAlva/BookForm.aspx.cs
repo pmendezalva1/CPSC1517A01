0e84a63b5d5e2d04e5147f0cac286e89a6c4f01a
pmendezalva1 <46577116+pmendezalva1@users.noreply.github.com>
2019-03-22 13:48:36 -0600

    ASP Test Done!

---
+++ Activity2/WebApp-PMendezAlva/BookForm.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp_PMendezAlva
+{
+    public partial class BookForm : System.Web.UI.Page
+    {
+        public static List<Books> bookInfo;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Message.Text = "";
+            if (!Page.IsPostBack)
+            {
+                bookInfo = new List<Books>();
+            }
+        }
+
+        protected void Add_Click(object sender, EventArgs e)
+        {
+            string isbn = ISBN.Text;
+            string name = Name.Text;
+            double cost = double.Parse(PurchaseCost.Text);
+            double price = double.Parse(SellingPrice.Text);
+
+            //if(bookInfo.Contains(isbn))
+            //{
+            //    Message.Text = "This book is already registered! Please pick a different ISBN.";
+            //}
+            
+            //else
+            //{
+                bookInfo.Add(new Books(isbn, name, cost, price));
+                Message.Text = "Your book has been registered!";
+            //}
+            BookListDisplay.DataSource = bookInfo;
+            BookListDisplay.DataBind();
+        }
+
+        protected void Clear_Click(object sender, EventArgs e)
+        {
+            ISBN.Text = "";
+            Name.Text = "";
+            PurchaseCost.Text = "";
+            SellingPrice.Text = "";
+            Message.Text = "Cleared!";
+
+            BookListDisplay.DataSource = null;
+            BookListDisplay.DataBind();
+        }
+    }
+}
---
+++ Activity2/WebApp-PMendezAlva/Books.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp_PMendezAlva
+{
+    public class Books
+    {
+        public string ISBN { get; set; }
+        public string Name { get; set; }
+        public double PurchaseCost { get; set; }
+        public double SellingPrice { get; set; }
+
+        public Books()
+        {
+
+        }
+        public Books(string isbn, string name, double cost, double price)
+        {
+            ISBN = isbn;
+            Name = name;
+            PurchaseCost = cost;
+            SellingPrice = price;
+        }
+    }
+}
